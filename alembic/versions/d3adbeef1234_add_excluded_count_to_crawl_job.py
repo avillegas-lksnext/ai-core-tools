d8ab3d7fc2e5158857f3de2da3ebfe9f5ecd07cf
Aitor Gil Chaves <a.gil@lksnext.com>
2026-07-15 16:53:21 +0200

    Version MattinAI (develop) 15/07/2026

--- alembic/versions/d3adbeef1234_add_excluded_count_to_crawl_job.py
+++ alembic/versions/d3adbeef1234_add_excluded_count_to_crawl_job.py
@@ -1,7 +1,7 @@
 """add excluded_count to crawl_job
 
 Revision ID: d3adbeef1234
-Revises: 
+Revises: merge001_userdel_platform_role
 Create Date: 2026-06-25 00:00:00.000000
 """
 from alembic import op
@@ -9,7 +9,8 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'd3adbeef1234'
-down_revision = None
+
+down_revision = 'merge001_userdel_platform_role'
 branch_labels = None
 depends_on = None
 
